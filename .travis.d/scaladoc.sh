bd36e3579e646bb47187a7e9ed1de87205aae956
radium226 <none@none.no>
2016-04-23 16:58:51 +0200

    Deploy to GitHub Pages

---
+++ .travis.d/scaladoc.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+mkdir -p "gh-pages"
+cp -a "target/scala-2.11/api/" "gh-pages/"
+cd "gh-pages"
+git init
+git config user.name "Travis CI"
+git config user.email "none@none.no"
+git add .
+git commit -m "Deploy to GitHub Pages"
+git push --force "https://radium226:${GITHUB_TOKEN}@github.com/radium226/mowitnow.git" "master:gh-pages"
